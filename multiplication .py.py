06412e21f7666af5919c49edb314e6f97e168067
Kiran <kirankalthireddy6@gmail.com>
2023-04-19 07:14:51 +0530

    Add files via upload

---
+++ multiplication .py.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[14]:
+
+
+#Write a function that inputs a number and prints the multiplication table of that number
+def multiplication (n = "enter the number:"):
+    for i in range(1,11):
+        print(n, "*", i, "=", n*i)
+multiplication(6)
+
+
+# In[12]:
+
+
+for i in range (10):
+    print(i)
+
+
+# In[ ]:
+
+
+
+
